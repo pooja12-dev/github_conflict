4b56b8f471fd9c0b5c958f1ddda5011aa2a8eaeb
pooja12-dev <poojafixl@gmail.com>
2025-06-19 16:45:22 +0530

    initial commit

---
+++ src/Content.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Content = () => {
+  return <main>Initial Main Content</main>;
+};
+
+export default Content;
---
+++ src/Header.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Header = () => {
+  return <header>Initial Header Content</header>;
+};
+
+export default Header;
