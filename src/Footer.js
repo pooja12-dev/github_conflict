bc5296661d60e240892357d9e812a213d82150fd
pooja12-dev <poojafixl@gmail.com>
2025-06-19 17:25:44 +0530

    changed files:

--- src/Footer.js
+++ src/Footer.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Footer = () => {
-  return <footer>Module 1</footer>;
+  return<> <footer>Module 1</footer>;
+  <h2>Modulessssssssss</h2>
+  </>
 };
 
 export default Footer;
+// This code defines a simple Footer component that returns a footer element with the text "Module 1".
+// It also includes an h2 element with the text "Modulessssss
