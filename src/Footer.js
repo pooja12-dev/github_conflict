06c9f5937e1989d10d01eb1419d916624e477588
pooja12-dev <poojafixl@gmail.com>
2025-06-19 16:53:45 +0530

    change footer in branch 1

--- src/Footer.js
+++ src/Footer.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Footer = () => {
-  return <footer>Iniccxcxccczxtial Footer Content</footer>;
+  return <footer>Iniccxcxccczxfgdfgdftial Footer Content</footer>;
 };
 
 export default Footer;
